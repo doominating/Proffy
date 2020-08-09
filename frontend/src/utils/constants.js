e3f46e63b5bda4554db05597b09fe563180bb19b
Afonso Amaro <doominating@gmail.com>
2020-08-09 00:01:00 -0300

    add utils constants week_days and subjects

---
+++ frontend/src/utils/constants.js
@@ -0,0 +1,22 @@
+export const WEEK_DAYS = [
+  { value: '0', label: 'Domingo' },
+  { value: '1', label: 'Segunda-feira' },
+  { value: '2', label: 'Terça-feira' },
+  { value: '3', label: 'Quarta-feira' },
+  { value: '4', label: 'Quinta-feira' },
+  { value: '5', label: 'Sexta-feira' },
+  { value: '6', label: 'Sábado' },
+];
+
+export const SUBJECTS = [
+  { value: 'Artes', label: 'Artes' },
+  { value: 'Biologia', label: 'Biologia' },
+  { value: 'CIências', label: 'CIências' },
+  { value: 'Educação física', label: 'Educação física' },
+  { value: 'Física', label: 'Física' },
+  { value: 'Geografia', label: 'Geografia' },
+  { value: 'História', label: 'História' },
+  { value: 'Matemática', label: 'Matemática' },
+  { value: 'Português', label: 'Português' },
+  { value: 'Química', label: 'Química' },
+];
