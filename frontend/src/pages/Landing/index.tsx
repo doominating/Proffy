357e944f6026a7e92dd1b5912c50c6acfb4bee47
Afonso Amaro <doominating@gmail.com>
2020-08-09 18:17:20 -0300

    tweaks and fixes in styles

--- frontend/src/pages/Landing/index.tsx
+++ frontend/src/pages/Landing/index.tsx
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import giveClassesIcon from '../../assets/images/icons/give-classes.svg';
 import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg';
 import studyIcon from '../../assets/images/icons/study.svg';
+import teachIcon from '../../assets/images/icons/teach.svg';
 import landingImg from '../../assets/images/landing.svg';
 import logoImg from '../../assets/images/logo.svg';
+import api from '../../services/api';
 import './styles.css';
 
 function Landing() {
+  const [totalConnections, setTotalConnections] = useState(0);
+
+  useEffect(() => {
+    api.get('/connections').then((response) => {
+      const { total } = response.data;
+
+      setTotalConnections(total);
+    });
+  }, []);
+
   return (
     <div id='page-landing'>
       <div id='page-landing-content' className='container'>
@@ -24,18 +35,18 @@ function Landing() {
 
         <div className='buttons-container'>
           <Link to='/study' className='study'>
-            <img src={studyIcon} alt='Estudar' />
-            Estudar
+            <img src={studyIcon} alt='Aprender' />
+            Aprender
           </Link>
 
-          <Link to='/give-classes' className='give-classes'>
-            <img src={giveClassesIcon} alt='Dar aulas' />
-            Dar aulas
+          <Link to='/teach' className='teach'>
+            <img src={teachIcon} alt='Ensinar' />
+            Ensinar
           </Link>
         </div>
 
         <span className='total-connections'>
-          Total de 200 conexões já realizadas
+          Total de {totalConnections} conexões já realizadas
           <img src={purpleHeartIcon} alt='Coração roxo' />
         </span>
       </div>
