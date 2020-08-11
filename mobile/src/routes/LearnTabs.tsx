d6e40bae37196823423f5fb85181f5b628bee56e
Afonso Amaro <doominating@gmail.com>
2020-08-11 14:09:45 -0300

    use pageheader component

--- mobile/src/routes/LearnTabs.tsx
+++ mobile/src/routes/LearnTabs.tsx
@@ -40,8 +40,14 @@ function LearnTabs() {
         component={TeacherList}
         options={{
           tabBarLabel: 'Proffys',
-          tabBarIcon: ({ color, size }) => {
-            return <Ionicons name='ios-easel' size={size} color={color} />;
+          tabBarIcon: ({ color, size, focused }) => {
+            return (
+              <Ionicons
+                name='ios-easel'
+                size={size}
+                color={focused ? '#8257e5' : color}
+              />
+            );
           },
         }}
       />
@@ -50,8 +56,14 @@ function LearnTabs() {
         component={Favorites}
         options={{
           tabBarLabel: 'Favoritos',
-          tabBarIcon: ({ color, size }) => {
-            return <Ionicons name='ios-heart' size={size} color={color} />;
+          tabBarIcon: ({ color, size, focused }) => {
+            return (
+              <Ionicons
+                name='ios-heart'
+                size={size}
+                color={focused ? '#8257e5' : color}
+              />
+            );
           },
         }}
       />
